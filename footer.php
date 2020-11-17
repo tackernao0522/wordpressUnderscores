42716c2f8bc4682024e5642e42972b7fcebc9176
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-17 12:16:28 +0900

    underscores+Bootstrapでレイアウト調整

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,24 +12,27 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'takabo' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'takabo' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'takabo' ), 'takabo', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+</div><!-- .row -->
+
+<footer id="colophon" class="site-footer">
+	<div class="site-info">
+		<a href="<?php echo esc_url(__('https://wordpress.org/', 'takabo')); ?>">
+			<?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			printf(esc_html__('Proudly powered by %s', 'takabo'), 'WordPress');
+			?>
+		</a>
+		<span class="sep"> | </span>
+		<?php
+		/* translators: 1: Theme name, 2: Theme author. */
+		printf(esc_html__('Theme: %1$s by %2$s.', 'takabo'), 'takabo', '<a href="http://underscores.me/">Underscores.me</a>');
+		?>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
