b8c90c0361daa0d273d4e6bdbae0f92589c20324
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-17 13:55:05 +0900

    カスタム投稿タイプを登録(register_post_type)

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * takabo functions and definitions
  *
@@ -7,12 +8,22 @@
  * @package takabo
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+add_action('init', function () {
+	register_post_type('item', [
+		'label' => '商品',
+		'public' => true,
+		'menu_position' => 10,
+		'menu_icon' => 'dashicons-store',
+		'supports' => ['thumbnail', 'title', 'editor'],
+	]);
+});
+
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
-if ( ! function_exists( 'takabo_setup' ) ) :
+if (!function_exists('takabo_setup')) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -20,17 +31,18 @@ if ( ! function_exists( 'takabo_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function takabo_setup() {
+	function takabo_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on takabo, use a find and replace
 		 * to change 'takabo' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'takabo', get_template_directory() . '/languages' );
+		load_theme_textdomain('takabo', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -38,19 +50,19 @@ if ( ! function_exists( 'takabo_setup' ) ) :
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', 'takabo' ),
+				'menu-1' => esc_html__('Primary', 'takabo'),
 			)
 		);
 
@@ -84,7 +96,7 @@ if ( ! function_exists( 'takabo_setup' ) ) :
 		);
 
 		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support('customize-selective-refresh-widgets');
 
 		/**
 		 * Add support for core custom logo.
@@ -102,7 +114,7 @@ if ( ! function_exists( 'takabo_setup' ) ) :
 		);
 	}
 endif;
-add_action( 'after_setup_theme', 'takabo_setup' );
+add_action('after_setup_theme', 'takabo_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -111,22 +123,24 @@ add_action( 'after_setup_theme', 'takabo_setup' );
  *
  * @global int $content_width
  */
-function takabo_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'takabo_content_width', 640 );
+function takabo_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('takabo_content_width', 640);
 }
-add_action( 'after_setup_theme', 'takabo_content_width', 0 );
+add_action('after_setup_theme', 'takabo_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function takabo_widgets_init() {
+function takabo_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'takabo' ),
+			'name'          => esc_html__('Sidebar', 'takabo'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'takabo' ),
+			'description'   => esc_html__('Add widgets here.', 'takabo'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -134,22 +148,23 @@ function takabo_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'takabo_widgets_init' );
+add_action('widgets_init', 'takabo_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function takabo_scripts() {
-	wp_enqueue_style( 'takabo-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'takabo-style', 'rtl', 'replace' );
+function takabo_scripts()
+{
+	wp_enqueue_style('takabo-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('takabo-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'takabo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('takabo-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'takabo_scripts' );
+add_action('wp_enqueue_scripts', 'takabo_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -174,7 +189,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
