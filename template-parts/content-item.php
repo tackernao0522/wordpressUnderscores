d3886ab507032efacbb9dca26dc5e74a1662cb68
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-19 18:20:29 +0900

    カスタムフィールドで配列を取得-get_post_meta-false

--- template-parts/content-item.php
+++ template-parts/content-item.php
@@ -2,6 +2,7 @@
 
 <?php $price = get_post_meta(get_the_ID(), '価格', true); ?>
 <?php $publisher = get_post_meta(get_the_ID(), '出版社', true); ?>
+<?php $writer = get_post_meta(get_the_ID(), '著者', false); ?>
 <dl>
   <?php if ($price !== '') : ?>
     <dt>価格</dt>
@@ -10,5 +11,11 @@
   <?php if ($publisher !== '') : ?>
     <dt>出版社</dt>
     <dd><?php echo esc_html($publisher); ?></dd>
+  <?php endif; ?>
+  <?php if ($writer) : ?>
+    <dt>著者</dt>
+    <?php foreach ($writer as $w) : ?>
+      <dd><?php echo esc_html($w); ?></dd>
+    <?php endforeach; ?>
   <?php endif; ?>
 </dl>
